2caf9ff60e03ac4562414835cf0a3cbb669c61db
mitz-lad <mitz-lad@users.noreply.github.com>
2021-02-23 17:06:08 +0000

    Added logging for data and urls. Added debt details and repayment details. Added activity content.

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,17 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// SET Breadcrumbs - useful for relative templates
+router.use('/', (req, res, next) => {
+    res.locals.currentURL = req.originalUrl; //current screen
+    res.locals.prevURL = req.get('Referrer'); // previous screen
+    console.log('previous page:: ' + res.locals.prevURL + '\ncurrent page: ' + req.url + ' ' + res.locals.currentURL );
+    next();
+    // if (!req.session.data['breadcrumbs']) {
+    //     req.session.data['breadcrumbs'] = [];
+    // }
+    
+    // req.session.data['breadcrumbs'].push({'title': 'page title', 'url': res.locals.prevURL});
+});
+
 module.exports = router
