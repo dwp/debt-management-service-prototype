5d6da2b1335b3dc6a6a1fc853906ecd0f317fdce
mitz-lad <mitz-lad@users.noreply.github.com>
2021-02-25 20:23:20 +0000

    More dynamic data driven for sprint scenario.

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -20,13 +20,169 @@ Example usage:
 module.exports = {
 
   // Insert values here
-  people: {
-    id: 1,
-    name: "David Ross",
-    nino: "RM 52 23 65 B",
-    onBenefit: true,
-    benefits: [ "Universal Credit" ]
-  }
+  "people": [
+    {
+      "id": 1,
+      "name": "David Ross",
+      "nino": "RM 52 23 65 B",
+      "onBenefit": true,
+      "benefits": [ 
+        "Universal Credit"
+        ],
+      "debts": [
+        {
+          "id": 1,
+          "title": "Budgeting Advance",
+          "originalBalance": 120.00,
+          "repaymentAmount": 10.00,
+          "debtStart": "18/01/2020"
+        },
+        {
+          "id": 2,
+          "title": "Universal Credit Advance (New Claims) 2",
+          "originalBalance": 300.00,
+          "repaymentAmount": 25.00,
+          "debtStart": "15/07/2020"
+        },
+        {
+          "id": 3,
+          "title": "Universal Credit Advance (New Claims) 1",
+          "originalBalance": 240.00,
+          "repaymentAmount": 20.00,
+          "debtStart": "06/07/2020"
+        }
+      ],
+      "repayments": [
+        {
+          "id:": 1,
+          "date": "12/02/2021",
+          "method": "Universal Credit Deduction",
+          "allocations": [
+            {
+              "debtId": 1,
+              "amount": 20.00
+            },
+            {
+              "debtId": 2,
+              "amount": 25.00
+            },
+            {
+              "debtId": 3,
+              "amount": 10.00
+            }
+          ]
+        },
+        {
+          "id:": 2,
+          "date": "12/01/2021",
+          "method": "Universal Credit Deduction",
+          "allocations": [
+            {
+              "debtId": 1,
+              "amount": 20.00
+            },
+            {
+              "debtId": 2,
+              "amount": 25.00
+            }
+          ]
+        },
+        {
+          "id:": 3,
+          "date": "12/12/2020",
+          "method": "Universal Credit Deduction",
+          "allocations": [
+            {
+              "debtId": 1,
+              "amount": 20.00
+            },
+            {
+              "debtId": 2,
+              "amount": 25.00
+            }
+          ]
+        },
+        {
+          "id:": 4,
+          "date": "12/11/2020",
+          "method": "Universal Credit Deduction",
+          "allocations": [
+            {
+              "debtId": 1,
+              "amount": 20.00
+            },
+            {
+              "debtId": 2,
+              "amount": 25.00
+            }
+          ]
+        },
+        {
+          "id:": 5,
+          "date": "12/10/2020",
+          "method": "Universal Credit Deduction",
+          "allocations": [
+            {
+              "debtId": 1,
+              "amount": 20.00
+            },
+            {
+              "debtId": 2,
+              "amount": 25.00
+            }
+          ]
+        },
+        {
+          "id:": 6,
+          "date": "12/09/2020",
+          "method": "Universal Credit Deduction",
+          "allocations": [
+            {
+              "debtId": 1,
+              "amount": 20.00
+            },
+            {
+              "debtId": 2,
+              "amount": 25.00
+            }
+          ]
+        },
+        {
+          "id:": 7,
+          "date": "12/08/2020",
+          "method": "Universal Credit Deduction",
+          "allocations": [
+            {
+              "debtId": 1,
+              "amount": 20.00
+            },
+            {
+              "debtId": 2,
+              "amount": 25.00
+            }
+          ]
+        },
+        
+      ]
+    },
+    {
+      "id": 2,
+      "name": "linda smith",
+      "nino": "RM 52 23 65 C",
+      "onBenefit": false,
+      "benefits": [ 
+        "Universal Credit",
+        "something"
+        ],
+      "debts": [
+        {
+          "title": "Universal Credit Advance",
+          "balance": 400.00
+        }
+      ]
+    }
+  ]
+ 
 
 
 }
--- app/routes.js
+++ app/routes.js
@@ -33,20 +33,6 @@ router.get('/multiple-advances-1/debt-summary', function (req, res, next) {
     next();
 })
 
-router.get('/multiple-advances-1/debt-summary', function (req, res, next) {
-
-    // reset breadcrumbs as this is current start
-    req.session.data.breadcrumbs = {};
-
-    // add summary to breadcrumbs
-    req.session.data.breadcrumbs.debtSummary = {
-        text: 'Debt Summary', 
-        url: '/multiple-advances-1/debt-summary' };
-
-    console.log('\nsession data:\n' + JSON.stringify(req.session.data, null, 2)) + '\n\n';
-    next();
-})
-
 router.get('/multiple-advances-1/debt-details/:debtId', function (req, res, next)  {
 
     // if already visited the repayemt details page, remove it from breadcrumbs
@@ -80,6 +66,35 @@ router.get('/multiple-advances-1/repayment-details/:repaymentId', function (req,
 
     console.log('\nsession data:\n' + JSON.stringify(req.session.data, null, 2)) + '\n\n';
     res.render('multiple-advances-1/repayment-details');
+})
+
+
+// ------------------------------------------
+
+router.get('/multiple-advances-2/person/:personId/debt-summary', function (req, res, next) {
+
+    // reset breadcrumbs as this is current start
+    // req.session.data.breadcrumbs = {};
+
+    // add summary to breadcrumbs
+    // req.session.data.breadcrumbs.debtSummary = {
+    //     text: 'Debt Summary', 
+    //     url: '/multiple-advances-1/debt-summary' };
+
+    console.log('\npersonId:' + req.params.personId + '\n');
+    res.render('multiple-advances-2/debt-summary.html', {
+        personId: req.params.personId
+    });
+})
+
+router.get('/multiple-advances-2/person/:personId/debt-details/:debtId', function (req, res, next) {
+
+    console.log('\npersonId:' + req.params.personId + '\n');
+    console.log('debtId:' + req.params.debtId + '\n');
+    res.render('multiple-advances-2/debt-details.html', {
+        personId: req.params.personId,
+        debtId: req.params.debtId
+    });
 })
 
 module.exports = router
