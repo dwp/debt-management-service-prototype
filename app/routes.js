180f08ab2e94c0ab02723f8247597460538743cc
mitz-lad <mitz-lad@users.noreply.github.com>
2021-02-26 08:55:24 +0000

    updated debt details and repayment details to be dynamic

--- app/routes.js
+++ app/routes.js
@@ -14,63 +14,93 @@ router.use('/', (req, res, next) => {
         req.session.data.breadcrumbs = [];
     }
 
-    console.log('\nsession data:\n' + JSON.stringify(req.session.data, null, 2)) + '\n\n';
+    // console.log('\nsession data:\n' + JSON.stringify(req.session.data, null, 2)) + '\n\n';
 
     next();
 });
 
-router.get('/multiple-advances-1/debt-summary', function (req, res, next) {
+// =========== Get details based on router params ===================
 
-    // reset breadcrumbs as this is current start
-    req.session.data.breadcrumbs = {};
+// get the person details from the session data and attach it to the request object
+router.param('personId', function (req, res, next, personId) {
+    
+    const person = req.session.data.people.find( ({ id }) => id == personId );
 
-    // add summary to breadcrumbs
-    req.session.data.breadcrumbs.debtSummary = {
-        text: 'Debt Summary', 
-        url: '/multiple-advances-1/debt-summary' };
-
-    console.log('\nsession data:\n' + JSON.stringify(req.session.data, null, 2)) + '\n\n';
-    next();
+    if (person) {
+        req.person = person
+        next()
+    } else {
+        next(new Error('failed to load person'))
+    }
 })
 
-router.get('/multiple-advances-1/debt-details/:debtId', function (req, res, next)  {
+// get debt details and associated repayments
+router.param('debtId', function (req, res, next, debtId) {
+    
+    const debt = req.person.debts.find( ({ id }) => id == debtId )
+    
+    if (debt) {
+        const repayments = []
 
-    // if already visited the repayemt details page, remove it from breadcrumbs
-    if ( 'debtDetails' in req.session.data.breadcrumbs) {
-        delete req.session.data.breadcrumbs.debtDetails
-    } 
+        for (const i in req.person.repayments) {
+            const repayment = req.person.repayments[i]
 
-    // add debt details to breadcrumbs
-    req.session.data.breadcrumbs.debtDetails = { 
-        text: 'debts', 
-        url: '/multiple-advances-1/debt-details',
-        id: req.params.debtId };
+            // get repayment data only related to this debt
+            repayment.allocations.filter(allocation => {
+                
+                if ( allocation.debtId == debt.id ) {
+                    repayments.push({
+                        'id': repayment.id,
+                        'date': repayment.date,
+                        'method': repayment.method, 
+                        'amount': allocation.amount});
+                }
+            })
 
-    console.log('\nsession data:\n' + JSON.stringify(req.session.data, null, 2)) + '\n\n';
+        }
 
-    res.render('multiple-advances-1/debt-details');
+        req.debt = debt
+        req.repayments = repayments
+        next()
+    } else {
+        next(new Error('failed to load debt'))
+    }
 })
 
-router.get('/multiple-advances-1/repayment-details/:repaymentId', function (req, res, next) {
+// get repayment details and associated debts
+router.param('repaymentId', function (req, res, next, repaymentId) {
+    
+    const repayment = req.person.repayments.find( ({ id }) => id == repaymentId )
+    const debts = []
 
-    // if already visited the repayemt details page, remove it from breadcrumbs
-    if ( 'repaymentDetails' in req.session.data.breadcrumbs) {
-        delete req.session.data.breadcrumbs.repaymentDetails
-    } 
+    if (repayment) {
 
-    // add repayment details to breadcrumbs
-    req.session.data.breadcrumbs.repaymentDetails = { 
-        text: 'repayments', 
-        url: '/multiple-advances-1/repayment-details',
-        id: req.params.repaymentId };
+        // get debt data only related to this repayment
+        for (const i in repayment.allocations) {
+            const allocation = repayment.allocations[i]
+            
+            req.person.debts.filter(debt => {
+                
+                if ( debt.id == allocation.debtId ) {
+                    debts.push({
+                        'id': debt.id,
+                        'title': debt.title,
+                        'amount': allocation.amount});
+                }
+            })
+        }
 
-    console.log('\nsession data:\n' + JSON.stringify(req.session.data, null, 2)) + '\n\n';
-    res.render('multiple-advances-1/repayment-details');
+        req.repayment = repayment
+        req.debts = debts
+        next()
+    } else {
+        next(new Error('failed to load debt'))
+    }
 })
 
+// =========== Routes ===================
 
-// ------------------------------------------
-
+// get person summary
 router.get('/multiple-advances-2/person/:personId/debt-summary', function (req, res, next) {
 
     // reset breadcrumbs as this is current start
@@ -81,19 +111,30 @@ router.get('/multiple-advances-2/person/:personId/debt-summary', function (req, 
     //     text: 'Debt Summary', 
     //     url: '/multiple-advances-1/debt-summary' };
 
-    console.log('\npersonId:' + req.params.personId + '\n');
     res.render('multiple-advances-2/debt-summary.html', {
-        personId: req.params.personId
+        Person: req.person
     });
 })
 
+//myArray.filter(x => x.id === '45').map(x => x.debts);
+
+// get debt details
 router.get('/multiple-advances-2/person/:personId/debt-details/:debtId', function (req, res, next) {
 
-    console.log('\npersonId:' + req.params.personId + '\n');
-    console.log('debtId:' + req.params.debtId + '\n');
     res.render('multiple-advances-2/debt-details.html', {
-        personId: req.params.personId,
-        debtId: req.params.debtId
+        Person: req.person,
+        Debt: req.debt,
+        Repayments: req.repayments
+    });
+})
+
+// get repayment details
+router.get('/multiple-advances-2/person/:personId/repayment-details/:repaymentId', function (req, res, next) {
+
+    res.render('multiple-advances-2/repayment-details.html', {
+        Person: req.person,
+        Repayment: req.repayment,
+        Debts: req.debts
     });
 })
 
